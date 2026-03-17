556018e32f7722e271d7654adfbe499eb729bdb6
Karol Gucwa <11karol.gucwa@gmail.com>
2026-03-17 18:11:15 +0100

    design database model
    created basic database model for storing csv files

--- datasets/models.py
+++ datasets/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Dataset(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    file = models.FileField(upload_to="datasets/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
