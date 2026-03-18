918d8d44b68f7ae654a7699257c8a4b85e986437
bartli555 <barlis3456@gmail.com>
2026-03-18 15:04:19 +0100

    Create Dataset List Page

--- datasets/urls.py
+++ datasets/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload_csv, name='upload_csv'),
+    path('list/', views.file_list, name='list'),
 ]
--- datasets/views.py
+++ datasets/views.py
@@ -4,3 +4,5 @@ def index(request):
     return render(request, "datasets/index.html")
 def upload_csv(request):
     return render(request, 'datasets/upload.html')
+def file_list(request):
+    return render(request, 'datasets/list.html')
