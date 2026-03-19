de1c2008046c0ca084406220c985c6a78279b8e5
bartli555 <barlis3456@gmail.com>
2026-03-19 14:22:20 +0100

    Create Dataset Detail View

--- datasets/views.py
+++ datasets/views.py
@@ -6,3 +6,8 @@ def upload_csv(request):
     return render(request, 'datasets/upload.html')
 def file_list(request):
     return render(request, 'datasets/list.html')
+def file_detail(request):
+    return render(request, 'datasets/detail.html')
+def detail(request, plik_id):
+    context = {'plik': {'id': plik_id, 'nazwa': f'Plik_nr_{plik_id}.csv'}}
+    return render(request, 'datasets/detail.html', context)
