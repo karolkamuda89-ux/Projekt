6ffac2b422ed279a7414eb7f72c1c69795e3321e
mateuszklima <matik.7.11@gmail.com>
2026-03-19 19:24:48 +0100

    Implement CSV Upload Functionality

--- data_explorer_project/settings.py
+++ data_explorer_project/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- datasets/urls.py
+++ datasets/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload_csv, name='upload_csv'),
     path('list/', views.file_list, name='list'),
-    path('list/', views.file_detail, name='detail'),
     path('detail/<int:plik_id>/', views.detail, name='detail'),
 ]
