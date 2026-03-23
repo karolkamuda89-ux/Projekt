4cbdfeda5494aa32dc36f6fe2a9707c2c2ab23b2
karolkamuda89-ux <karolkamuda89@gmail.com>
2026-03-23 20:53:58 +0100

    adding login page with use django auth system(in progress)

--- datasets/urls.py
+++ datasets/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload_csv, name='upload_csv'),
     path('list/', views.file_list, name='list'),
     path('detail/<int:plik_id>/', views.detail, name='detail'),
+    path('login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
 ]
