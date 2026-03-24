2a93316b98f9e868f5cdf9115a2db7eb4ee6428e
Karol Gucwa <11karol.gucwa@gmail.com>
2026-03-24 22:08:30 +0100

    test upload csv

--- datasets/views.py
+++ datasets/views.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import csv
 from django.shortcuts import redirect, render, get_object_or_404
 
@@ -5,6 +6,28 @@ from datasets.models import Dataset
 def detail(request, plik_id):
     d = get_object_or_404(Dataset, id=plik_id)
     return render(request, 'datasets/detail.html', {'plik': d})
+=======
+from django.shortcuts import render, redirect
+from django import forms
+from .utils import get_statistics
+from .models import Dataset
+from django.shortcuts import get_object_or_404
+>>>>>>> Stashed changes
+
+class DatasetForm(forms.ModelForm):
+    class Meta:
+        model = Dataset
+        fields = ['name', 'description', 'file']
+
+def upload_csv(request):
+    if request.method == 'POST':
+        form = DatasetForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = DatasetForm()
+    return render(request, 'datasets/upload.html', {'form': form})
 
 def index(request):
     return render(request, "datasets/index.html")
