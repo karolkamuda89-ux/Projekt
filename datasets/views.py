89bea56be1c8bf8dc20c81fd5ee0241b1a16d5a4
Karol Gucwa <11karol.gucwa@gmail.com>
2026-03-24 18:07:44 +0100

    implement basic statistics (update)

--- datasets/views.py
+++ datasets/views.py
@@ -1,29 +1,18 @@
-import csv
-from django.shortcuts import redirect, render, get_object_or_404
-
-from datasets.models import Dataset
-def detail(request, plik_id):
-    d = get_object_or_404(Dataset, id=plik_id)
-    return render(request, 'datasets/detail.html', {'plik': d})
+from django.shortcuts import render
+from .utils import get_statistics
+from .models import Dataset
+from django.shortcuts import get_object_or_404
 
 def index(request):
     return render(request, "datasets/index.html")
+
 def upload_csv(request):
-    if request.method == 'POST' and request.FILES.get('file'):
-        name = request.POST.get('title', 'Untilted')
-        description = request.POST.get('description', 'none')
-        file = request.FILES['file']
-        
-        # Zapis do bazy danych
-        Dataset.objects.create(name=name, description=description, file=file)
-        return redirect('list')  # Przekierowanie na listę po wgraniu
     return render(request, 'datasets/upload.html')
+
 def file_list(request):
-    pliki = Dataset.objects.all().order_by('-uploaded_at')
-    return render(request, 'datasets/list.html', {'pliki': pliki})
-def file_detail(request):
-    return render(request, 'datasets/detail.html')
-def detail(request, plik_id):
-    plik = get_object_or_404(Dataset, id=plik_id)
-    return render(request, 'datasets/detail.html', {'plik': plik})
-    
+    return render(request, 'datasets/list.html')
+
+def dataset_detail(request, dataset_id):
+    dataset = get_object_or_404(Dataset, id=dataset_id)
+    stats = get_statistics(dataset.file.path)
+    return render(request, 'datasets/detail.html', {'dataset': dataset, 'stats': stats})
